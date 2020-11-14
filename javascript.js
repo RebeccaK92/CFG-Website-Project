d4c16e1ce6303c3aa60afb3d54656568e4679ea6
RebeccaK92 <71036150+RebeccaK92@users.noreply.github.com>
2020-11-14 16:57:29 +0000

    contact form - maybe working

--- javascript.js
+++ javascript.js
@@ -1,1 +1,7 @@
-document.addEventListener('DOMContentLoaded',function() 
+
+function sendEmail() {
+    var name = $('#contact #name').val();
+    var email = $('#contact #email').val();
+    var message = $('#contact textarea').val();
+    window.location.href = 'mailto:mail@company.com?subject=The subject - ' + name + ' (' + email + ')' + '&body=' + message;
+};
