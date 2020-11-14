d94ff025785e87eff343f5f9f519ecc247cf171a
RebeccaK92 <71036150+RebeccaK92@users.noreply.github.com>
2020-11-14 20:56:48 +0000

    Contact working

--- javascript.js
+++ javascript.js
@@ -1,6 +1,7 @@
 
 function sendEmail() {
-        var email = $('#contact #email').val();
+    console.log("yee");
+    var email = $('#contact #name').val();
     var message = $('#contact textarea').val();
-    window.location.href = 'mailto:snoopyrox@btconnect.com?subject=Comments ' + name + ' (' + email + ')' + '&body=' + message;
+    window.location.href = 'mailto:cfg.projectrl@gmail.com?subject=Comments ' + name + ' (' + email + ')' + '&body=' + message;
 };
