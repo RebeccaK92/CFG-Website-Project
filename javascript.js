69612b146eb64c805a9292fee02ca68958527fa0
RebeccaK92 <71036150+RebeccaK92@users.noreply.github.com>
2020-11-14 16:59:15 +0000

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,7 +1,6 @@
 
 function sendEmail() {
-    var name = $('#contact #name').val();
-    var email = $('#contact #email').val();
+        var email = $('#contact #email').val();
     var message = $('#contact textarea').val();
-    window.location.href = 'mailto:mail@company.com?subject=The subject - ' + name + ' (' + email + ')' + '&body=' + message;
+    window.location.href = 'mailto:snoopyrox@btconnect.com?subject=Comments ' + name + ' (' + email + ')' + '&body=' + message;
 };
